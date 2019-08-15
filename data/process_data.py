20f7af597d125c18a219bdf99a4a498d5f9f05a9
Christopher <christopher.hogendijk@gmail.com>
2019-08-15 11:03:15 +0000

    correct table name

--- data/process_data.py
+++ data/process_data.py
@@ -40,7 +40,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('InsertTableName', engine, index=False)
+    df.to_sql('disaster_table', engine, index=False)
 
 def main():
     if len(sys.argv) == 4:
